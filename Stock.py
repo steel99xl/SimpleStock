889020c58458c5d35445482e16fa770c832c66c7
steel99xl <steel99xl@gmail.com>
2021-03-11 00:16:21 -0500

    Add Short intrest/ bug fix
    Added list of highest short amounts
    fixed variables and added missing import

--- Stock.py
+++ Stock.py
@@ -3,21 +3,51 @@
 ## Imports
 import urllib
 import requests
+import sys
 
 
+## Define urls
+HighShort = "https://www.highshortinterest.com/"
+Iborrowdesk = "https://iborrowdesk.com/api/ticker/"
+YahooP1 = "https://query1.finance.yahoo.com/v8/finance/chart/"
+YahooP2 = "?region=US&lang=en-US&includePrePost=false&interval=1d&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance"
 
-# load urls from config file
-# TODO : ^
-Base = "https://iborrowdesk.com/api/ticker/"
-PUrl = "https://query1.finance.yahoo.com/v8/finance/chart/"
-PUrl2 = "?region=US&lang=en-US&includePrePost=false&interval=1d&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance"
+
+## Get URL response
+def Url(URL):
+    WebPage = urllib.request.urlopen(URL)
+    Data = str(WebPage.read())
+    return Data
+
+
+## Get list of largest shorts
+def Short():
+    Info = []
+    Data = Url(HighShort)
+    Data = Data.split('</tr>')
+    #from 4 - 44
+    i = 4
+    while(i <= 44):
+        if(i == 29):
+            i += 1
+            continue
+        Buff = Data[i]
+        Buff = Buff.split('>')
+        Ticker = Buff[3].strip('</a')
+        Per = Buff[10].strip('</td')
+        Out = Ticker + " - " + Per
+        Info.append(Out)
+        i += 1
+
+    x = 0
+    while(x <= len(Info)-4):
+        print(Info[x] +' | ' + Info[x+1] +' | ' + Info[x+2] +' | ' + Info[x+3])
+        x += 4
 
 
 ## Get Average Avalible Volume
 def Avg(Ticker):
-    URL = Base + Ticker.upper()
-    WebPage = urllib.request.urlopen(URL)
-    Data = str(WebPage.read())
+    Data = Url(Iborrowdesk + Ticker.upper())
     Data = Data.split('[')
     AData = Data[1].split('{')
     BData = Data[2].split('{')
@@ -30,7 +60,8 @@ def Avg(Ticker):
         i += 1
 
     print("Avg Avalible Volume for today = "+ str(Total/i))
-
+    i = 1
+    Total = 0
     while(i < len(BData)):
         X = BData[i].split(':')
         X = X[1].split(',')
@@ -42,9 +73,7 @@ def Avg(Ticker):
 
 ## Get Open, Current, and Previous Close price
 def Price(Ticker):
-    URL = PUrl + Ticker.upper() + PUrl2
-    WebPage = urllib.request.urlopen(URL)
-    Data = str(WebPage.read())
+    Data = Url(YahooP1 + Ticker.upper() + YahooP2)
     PData = Data.split(',')
     CPrice = PData[9].split(':')[1]
     YPrice = PData[10].split(':')[1]
@@ -61,10 +90,14 @@ def main(argv):
     if(len(argv) <= 0):
         print("Stock.py [TICKER] <options>")
         print(' -a             Get average volume of avalible shares')
+        print(' -s               Get list of the most shorted shares')
         return 1
+
+
+    if(argv[0] == "-s"):
+        Short()
+
     Ticker = argv[0]
-
-
     # lazy way of cheking for args
     try:
         Option = argv[1]
@@ -72,7 +105,8 @@ def main(argv):
         Option = ""
     if(Option == "-a"):
         Avg(Ticker)
-    Price(Ticker)
+
+    if(Ticker != Args[0]):
+        Price(Ticker)
 
 main(sys.argv[1:])
-
