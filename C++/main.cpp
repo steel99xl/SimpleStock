dbf8be1a53a62ee640ad5c22fd5d077ce5ef9990
steel99xl <steel99xl@gmail.com>
2021-05-20 16:03:46 -0400

    File IO/ Market
    added File IO and the Market

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <thread>
-#include <chrono>
 #include <cstring>
 #include <fstream>
+//#include <thread>
+//#include <chrono>
 
 // Only external library
 #include <curl/curl.h>
@@ -145,10 +145,168 @@ std::vector<std::string> LoadPortfolio(char *FilePath) {
     return lines;
 }
 
+void SavePortfolio(char *FilePath, std::vector<std::string> names,std::vector<std::string> amount, std::vector<std::string> price, std::vector<std::string> floatcash){
+    std::ofstream File;
 
- std::string MenuePortfolio(){
+    File.open(FilePath, std::ios::out);
+    if(File.is_open()){
+        File << "FloatCash="+floatcash[0]+"\n";
+        for(int i = 0; i < names.size(); i++){
+            File << names[i] + "," + amount[i] + "," + price[i] + "\n";
+        }
+        File.close();
+    }
+    std::cout << "SAVE complete..." << std::endl;
+}
+
+void ParserPortfolio(std::vector<std::string> lines, std::vector<std::string> *names, std::vector<std::string> *amount, std::vector<std::string> *price, std::vector<std::string> *floatcash){
+    std::string delims = ",=";
+    int delcount[5] = {0,0,0,0,0};
+    std::string data;
+
+    names->clear();
+    amount->clear();
+    price->clear();
+    floatcash->clear();
+
+    // 3 buffers cause we need three diffrent bits of info and it makes it easy
+    for(auto & line : lines) {
+        delcount[0] = 0;
+        delcount[1] = 0;
+        for (int j = 0; j < line.length(); j++) {
+            if (line[j] == delims[0]) {
+                if (delcount[0] == 0) {
+                    delcount[0] = j;
+                } else {
+                    delcount[1] = j;
+                }
+            }
+            if (line[j] == delims[1]) {
+                delcount[2] = j;
+                // ya
+                delcount[3] = delcount[4];
+            }
+        }
+        if (delcount[0] != delcount[1]) {
+            names->push_back(line.substr(0, delcount[0]));
+            amount->push_back(line.substr(delcount[0] + 1, delcount[1] - delcount[0] - 1));
+            price->push_back(line.substr(delcount[1] + 1, line.length() - 1));
+        }
+        delcount[4] += 1;
+    }
+
+    if(delcount[2] != 0){
+        floatcash->push_back(lines[delcount[3]].substr(delcount[2] + 1,lines[delcount[3]].length() - 1));
+    }
+    else{
+        floatcash->push_back("0.0");
+    }
+
+}
+
+void MarketPortfolio(std::vector<std::string> userinput, std::vector<std::string> *names, std::vector<std::string> *amount, std::vector<std::string> *price, std::vector<std::string> *floatcash ){
+    std::vector<std::string> tmpnames = *names;
+    std::vector<std::string> tmpprice = *price;
+    std::vector<std::string> tmpamount = *amount;
+    std::string *output;
+    std::string data;
+    char ticker[8];
+
+    std::strcpy(ticker, userinput[1].c_str());
+    data = WebHandler(ticker);
+    output = WebParser(data);
+    std::vector<std::string> cash = floatcash[0];
+    floatcash->pop_back();
+
+    if(std::strcmp(userinput[0].c_str(),"BUY") == 0) {
+
+        if (std::stof(cash[0]) < std::stof(output[0]) * std::stof(userinput[2])) {
+            std::cout << "ERROR low funds.." << std::endl;
+            return;
+        }
+        floatcash->push_back(std::to_string(std::stof(cash[0]) - std::stof(output[0]) * std::stof(userinput[2])));
+
+        for (int i = 0; i < names->size(); i++) {
+            // Error happend down here some where...
+
+            if (tmpnames[i] == userinput[1]) {
+
+                tmpprice[i] = std::to_string(((std::stof(tmpprice[i]) * std::stof(tmpamount[i]) +
+                                               std::stof(output[0]) * std::stof(userinput[2])) /
+                                              (std::stof(tmpamount[i]) + std::stof(userinput[2]))));
+                tmpamount[i] = std::to_string(std::stof(tmpamount[i]) + std::stof(userinput[2]));
+
+                price->clear();
+                amount->clear();
+
+                for (int j = 0; j < tmpamount.size(); j++) {
+                    price->push_back(tmpprice[j]);
+                    amount->push_back(tmpamount[j]);
+                }
+                std::cout << "PURCHASE complete..." << std::endl;
+                return;
+            }
+        }
+        names->push_back(userinput[1]);
+        price->push_back(output[0]);
+        amount->push_back(userinput[2]);
+        std::cout << "PURCHASE complete..." << std::endl;
+        return;
+    }
+
+    if(std::strcmp(userinput[0].c_str(),"SELL") == 0){
+
+        for (int i = 0; i < names->size(); i++) {
+            // Error happend down here some where...
+
+            if (tmpnames[i] == userinput[1]) {
+                if((std::stof(tmpamount[i]) - std::stof(userinput[2])) < 0){
+                    std::cout << "ERROR not enough shares..." << std::endl;
+                    return;
+                }
+
+                tmpamount[i] = std::to_string(std::stof(tmpamount[i]) - std::stof(userinput[2]));
+
+                price->clear();
+                amount->clear();
+
+                if(std::stof(tmpamount[i]) == 0){
+                    tmpamount.erase(tmpamount.begin()+i);
+                    tmpprice.erase(tmpprice.begin()+i);
+                    tmpnames.erase(tmpnames.begin()+i);
+
+                    names->clear();
+                    for(int j = 0; j < tmpnames.size(); j++){
+                        names->push_back(tmpnames[j]);
+                    }
+                }
+
+                for (int j = 0; j < tmpamount.size(); j++) {
+                    price->push_back(tmpprice[j]);
+                    amount->push_back(tmpamount[j]);
+                }
+                floatcash->push_back(std::to_string(std::stof(cash[0]) + std::stof(output[0]) * std::stof(userinput[2])));
+                std::cout << "SALE complete..." << std::endl;
+                return;
+            }
+        }
+
+        std::cout << "ERROR you dont own any " << userinput[1] << std::endl;
+        return;
+    }
+
+
+}
+
+std::string UserInput(){
     std::string userinput;
-    std::cout << "RELOAD | DISPLAY | EXIT" << std::endl;
+    std::cout << "# ";
+    std::cin >> userinput;
+    return userinput;
+}
+
+std::string UserAutoCap(){
+    std::string userinput;
 
     std::cout << "# ";
     std::cin >> userinput;
@@ -160,58 +318,35 @@ std::vector<std::string> LoadPortfolio(char *FilePath) {
     return userinput;
 }
 
-void DisplayPortfolio(std::vector<std::string> lines){
+void DisplayPortfolio(std::vector<std::string> names, std::vector<std::string> amount, std::vector<std::string> price, std::vector<std::string> floatcash){
     // Calculates informaion based on loaded file
     float LossGain = 0.0;
     float TotalValue = 0.0;
-    std::string delims = ",";
-    int delcount[2] = {0,0};
+    char ticker[8];
     std::string data;
-    char ticker[6];
 
-    // oooo auto thanks clion
     std::string *output;
     // 3 buffers cause we need three diffrent bits of info and it makes it easy
-    std::string name;
-    std::string amount;
-    std::string price;
     std::cout << "TICKER | Amount | Buy Price | Current Price | Loss/Gain \n" << std::endl;
-    for(auto & line : lines){
-        delcount[0] = 0;
-        delcount[1] = 0;
-        for(int j = 0; j < line.length(); j++){
-            if(line[j] == delims[0]){
-                if(delcount[0] == 0){
-                    delcount[0] = j;
-                }
-                else{
-                    delcount[1] = j;
-                }
+    for(int i = 0; i < names.size(); i++){
 
-            }
-        }
-        if(delcount[0] != delcount[1]){
-            name = line.substr(0,delcount[0]);
-            amount = line.substr(delcount[0]+1,delcount[1]-delcount[0]-1);
-            price = line.substr(delcount[1]+1,line.length()-1);
-
-            std::strcpy(ticker,name.c_str());
+            std::strcpy(ticker,names[i].c_str());
             data = WebHandler(ticker);
 
             output = WebParser(data);
 
-            LossGain += std::stof(amount) * std::stof(output[0]) - std::stof(amount) * std::stof(price);
-            TotalValue += std::stof(amount) * std::stof(output[0]);
+            LossGain += std::stof(amount[i]) * std::stof(output[0]) - std::stof(amount[i]) * std::stof(price[i]);
+            TotalValue += std::stof(amount[i]) * std::stof(output[0]);
 
-            std::cout << name << " | " << amount << " | " << price << " | " << output[0] << " | " << std::stof(output[0]) - std::stof(price) << std::endl;
-        }
+            std::cout << names[i] << " | " << amount[i] << " | " << price[i] << " | " << output[0] << " | " << std::stof(output[0]) - std::stof(price[i]) << std::endl;
     }
     std::cout << "\n\r------------------------------------------------" << std::endl;
-    std::cout << "Initial Value : $" << TotalValue - LossGain << std::endl;
+    std::cout << "Float Cash : $" << std::stof(floatcash[0]) << std::endl;
+    std::cout << "Initial Value : $" << TotalValue - LossGain + std::stof(floatcash[0]) << std::endl;
     std::cout << "Loss/Gain : $" << LossGain << std::endl;
-    std::cout << "Total value : $" << TotalValue << std::endl;
-
+    std::cout << "Total value : $" << TotalValue + std::stof(floatcash[0]) << std::endl;
     std::cout << "\n" << std::endl;
+
 
 }
 
@@ -229,40 +364,73 @@ int main(int argc, char *argv[]){
 
     // Capitalizes user input
     int max = std::strlen(argv[1]);
-    char userinput[max];
+    char input[max];
     int i = 0;
     while (argv[1][i]){
-        userinput[i] = toupper(argv[1][i]);
+        input[i] = toupper(argv[1][i]);
         i++;
     }
 
     if(std::strcmp(argv[1], "-f") == 0 && argc == 3){
-        // Load and display file in one call
+        // This could be a seperate function
         bool View = true;
         std::vector<std::string> lines;
-        std::vector<std::string> buffer;
+        std::vector<std::string> names;
+        std::vector<std::string> amount;
+        std::vector<std::string> price;
+        std::vector<std::string> floatcash;
+        std::vector<std::string> userinput;
 
+        // Load Profile
         lines = LoadPortfolio(argv[2]);
-        std::string mode;
+        ParserPortfolio(lines,&names,&amount,&price,&floatcash);
+        DisplayPortfolio(names,amount,price,floatcash);
 
         while(View) {
-            mode = MenuePortfolio();
-            if(std::strcmp(mode.c_str(),"EXIT") == 0){
+
+            std::cout << "RELOAD | DISPLAY | BUY | SELL | SAVE | EXIT" << std::endl;
+            userinput.push_back(UserAutoCap());
+
+            if(std::strcmp(userinput[0].c_str(),"EXIT") == 0){
                 std::cout << "EXITING app..." << std::endl;
                 View = false;
             }
-            if(std::strcmp(mode.c_str(),"DISPLAY") == 0){
-                DisplayPortfolio(lines);
+            if(std::strcmp(userinput[0].c_str(),"DISPLAY") == 0){
+                DisplayPortfolio(names,amount,price,floatcash);
             }
-            if(std::strcmp(mode.c_str(),"RELOAD") == 0){
+            // DEBUG DISPLAY TO PRINT BUFFER ONLY
+            if(std::strcmp(userinput[0].c_str(),"DEVDISPLAY") == 0){
+                DisplayPortfolio(names,amount,price,floatcash);
+            }
+            if(std::strcmp(userinput[0].c_str(),"RELOAD") == 0){
                 lines = LoadPortfolio(argv[2]);
-                DisplayPortfolio(lines);
+                ParserPortfolio(lines,&names,&amount,&price,&floatcash);
+                DisplayPortfolio(names,amount,price,floatcash);
             }
+            if(std::strcmp(userinput[0].c_str(),"BUY") == 0 || std::strcmp(userinput[0].c_str(),"SELL") == 0){
+                std::cout << userinput[0] <<" MENU" << std::endl;
+                std::cout << "Enter Ticker Name" << std::endl;
+                userinput.push_back(UserAutoCap());
+                std::cout << "Enter Amount" << std::endl;
+                userinput.push_back(UserInput());
+
+                MarketPortfolio(userinput,&names,&amount,&price,&floatcash);
+
+                userinput.pop_back();
+                userinput.pop_back();
+
+            }
+            if(std::strcmp(userinput[0].c_str(),"SAVE") == 0){
+                std::cout << "SAVEING to file..." << std::endl;
+                SavePortfolio(argv[2],names,amount,price,floatcash);
+            }
+
+            userinput.pop_back();
         }
         return 0;
     }
 
-    data = WebHandler(userinput);
+    data = WebHandler(input);
 
    // for(int count = 0;count < 10; ++count){
    //     ProgressBar(count, 10, '#');
